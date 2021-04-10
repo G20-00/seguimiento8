fa74d0ec52cfadd7161578e242281c7fd5ec31ef
unknown <70019070+G20-00@users.noreply.github.com>
2021-04-10 00:09:59 -0500

    Final
    finished

--- test/model/MiniMarketTest.java
+++ test/model/MiniMarketTest.java
@@ -8,46 +8,60 @@ import org.junit.jupiter.api.Test;
 
 class MiniMarketTest {
 	private MiniMarket minimarket;
+	private String id;
+	private String typeId;
 	public void setupScenary1() {
 		minimarket = new MiniMarket();
+		id = "16691344";
+		typeId ="CC";
 	}
+	public void setupScenary3() {
+		minimarket = new MiniMarket();
+		id = "12312133";
+		typeId ="CC";
+	}
+	public void setupScenary2() {
+		minimarket = new MiniMarket();
+		id = "12312124";
+		typeId ="TI";
+	}
+	
 	
 	@Test
 	void testMiniMarket() {
 		setupScenary1();
 		
 		try {			
-			 minimarket.creatPerson("CC","16691344");			
+			 minimarket.creatPerson(typeId,id);			
 		
 		} catch (IdException nope) {
 			fail("Not IdException expected");
 		}catch(DayIncorrect nope) {
-			fail("Not DayIncorrect expected");
 		}
 	}
 	@Test
 	void testMiniMarket2() {
-		setupScenary1();
+		setupScenary2();
 		
 		try {			
-			 minimarket.creatPerson("TI","12312124");			
+			 minimarket.creatPerson(typeId,id);			
 		} catch (IdException nope) {
-			fail("Not IdException expected");
-		}catch(DayIncorrect nope) {
+			
+		}catch(DayIncorrect e) {
 			fail("Not DayIncorrect expected");
 		}
 	}
 	@Test
 	void testMiniMarket3() {
-		setupScenary1();
+		setupScenary3();
 		
 		try {			
-			 minimarket.creatPerson("CC","12312133");			
+			 minimarket.creatPerson(typeId,id);			
 		
 		} catch (IdException nope) {
 			fail("Not IdException expected");
-		}catch(DayIncorrect nope) {
-			fail("Not DayIncorrect expected");
+		}catch(DayIncorrect e) {
+			
 		}
 	}
 
